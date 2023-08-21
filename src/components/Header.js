11074e9bab5b3b2db2c9cfdc9175355dd1853cfb
unknown <brannongarrett77@gmail.com>
2023-08-21 19:10:46 +0200

    Added a button with a link to the booking page

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,6 +5,12 @@ const Header = () => {
         <header className="header">
             <section>
                 {/* Banner texts */}
+                <div className="banner">
+                    <h2>Little Lemon</h2>
+                    <h3>Chicago</h3>
+                    <p>We are a family owned Mediterranean restaurant, focused on traditional recipes served with a modern twist.</p>
+                    <a href="/booking"><button aria-label="On Click">Reserve a table</button></a>
+                </div>
             </section>
         </header>
     );
