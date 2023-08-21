9f859bc6d0c63295e6dce0658c3f358478b020e3
unknown <brannongarrett77@gmail.com>
2023-08-21 17:25:43 +0200

    Updated Header.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
+import Header from './components/Header';
 import Nav from './components/Nav';
 
 function App() {
   return (
     <>
       <Nav />
-
+      <Header />
     </>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Header = () => {
+    return (
+        <header className="header">
+            <section>
+                {/* Banner texts */}
+            </section>
+        </header>
+    );
+};
+
+export default Header;
