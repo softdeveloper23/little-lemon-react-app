7d211d400f98c76f282783a77ff7869aff27e5ba
unknown <brannongarrett77@gmail.com>
2023-08-22 16:20:15 +0200

    Added the Menu component

---
+++ src/components/Menu.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Menu = () => {
+    return (
+        <div className="menu-container">
+            <div>
+                <h2>This Week's Specials!</h2>
+            </div>
+        </div>
+    )
+}
+
+export default Menu;
