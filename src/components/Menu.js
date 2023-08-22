0affaf476b216655ef8c0db0fe8ed3f85339f51d
unknown <brannongarrett77@gmail.com>
2023-08-22 16:38:18 +0200

    Added buttons and cards

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,4 +1,5 @@
 import React from "react";
+import recipes from "../recipes";
 
 const Menu = () => {
     return (
@@ -7,8 +8,25 @@ const Menu = () => {
                 <h2>This Week's Specials!</h2>
                 <button>Order From The Menu</button>
             </div>
+            {/* Menu cards go here */}
+            <div className="cards">
+                {
+                    recipes.map(recipe => <div key={recipe.id}>
+                        <img src={recipe.image} alt={recipe.title} />
+                        <div>
+                            <div>
+                                <h5>{recipe.title}</h5>
+                                <p>{recipe.price}</p>
+                            </div>
+                            <p>{recipe.description}</p>
+                            <button className="orderBtn">Place An Order</button>
+                        </div>
+                    </div>)
+                }
+
+            </div>
         </div>
-    )
-}
+    );
+};
 
 export default Menu;
