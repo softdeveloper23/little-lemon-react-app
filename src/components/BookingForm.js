78e8925824cc3bf7c4fbaa6d581cd5232b796c9c
unknown <brannongarrett77@gmail.com>
2023-08-22 14:08:37 +0200

    Added code

--- src/components/BookingForm.js
+++ src/components/BookingForm.js
@@ -28,13 +28,34 @@ const BookingForm = () => {
                         {/* For time selection */}
                         <div>
                             <label htmlFor="book-time">Choose A Time: </label>
-                            <select id="book-time">
+                            <select id="book-time" value={times} onChange={(e) => setTimes(e.target.value)}>
                                 <option value="">Select A Time</option>
                                 {
                                     props.availableTimes.availableTimes.map(availableTimes => { return <option key={availableTimes}>{availableTimes}</option> })
                                 }
                             </select>
                         </div>
+
+                        {/* For number of guests */}
+                        <div>
+                            <label htmlFor="book-guests">Number Of Guests: </label>
+                            <input id="book-guests" min='1' value={guests} onChange={(e) => setGuests(e.target.value)} />
+                        </div>
+
+                        {/* Occasion field */}
+                        <div>
+                            <label htmlFor="book-occasion">Occasion: </label>
+                            <select id="book-occasion" key={occasion} value={occasion} onChange={(e) => setOccasion(e.target.value)}>
+                                <option>Birthday</option>
+                                <option>Anniversary</option>
+                            </select>
+                        </div>
+                        {/* Submit button */}
+                        <div className="btnReceive">
+                            <input aria-label="On Click" type="submit" value={"Make Your Reservation"} />
+                        </div>
+
+
                     </fieldset>
                 </form>
             </section>
